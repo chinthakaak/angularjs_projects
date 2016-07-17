3e6b035941ce03840fe85c103d5c359318e01668
kushan_chinthaka <kushan_chinthaka@SLL06910_AePONA.persistent.co.in>
2016-07-17 19:16:28 +0530

    Add conv aPP

---
+++ conversation-app/js/app-config.js
@@ -0,0 +1,7 @@
+const MQTT_HOST = "lr6id6.messaging.internetofthings.ibmcloud.com";
+const MQTT_PORT = 1883;
+const MQTT_PATH = '/';
+const MQTT_CLIENT_ID = "d:lr6id6:devicetype1:deviceid1";
+const USER_NAME = "use-token-auth";
+const PASSWORD = "LNvs(k?CZ_u@RWQG9W";
+const CMD_TOPIC = "iot-2/cmd/display/fmt/json";
---
+++ conversation-app/js/app-mqtt-provider.js
@@ -0,0 +1,34 @@
+function connect_mqtt(){
+		
+		// Create a client instance: Broker, Port, Websocket Path, Client ID
+		client = new Paho.MQTT.Client(MQTT_HOST, Number(MQTT_PORT), MQTT_PATH, MQTT_CLIENT_ID);
+	    
+		console.log("Client instantiated.")
+	    console.log("Now trying to connect...")
+		    
+		// set callback handlers
+		client.onConnectionLost = function (responseObject) {
+		    console.log("Connection Lost: "+responseObject.errorMessage);
+		}
+		 
+		client.onMessageArrived = function (message) {
+		  console.log("Message Arrived: "+message.payloadString);
+		  var scope = angular.element(document.getElementById("DisplayControllerId")).scope();
+		  	scope.$apply(function () {
+		  		scope.addDialog(JSON.parse(message.payloadString))
+		  	});
+		}
+		
+		// Connect the client, providing an onConnect callback
+		client.connect({
+		    onSuccess: onConnect,
+		    userName : USER_NAME,
+		    password : PASSWORD,
+		});
+		
+		// Called when the connection is made
+		function onConnect(){
+		    console.log("Connected!");
+			client.subscribe(CMD_TOPIC);
+		}	
+}
